72f69a230768595384396bcadd23a35cab8a9af5
Elias <52203697+elias-baumann@users.noreply.github.com>
2021-10-29 16:13:28 +0200

    update

--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ let geodata2 = []; //water
 let geodata3 = []; //accident
 let geodata4 = []; //bank
 
+let currentYear;
+let record = true;
+
 let backgroundImg;
 
 let bounds = {
@@ -31,6 +34,7 @@ function preload() {
 
   // backgroundImg = loadImage("bg.png"); // Foto mit allen Daten
   // backgroundImg = loadImage("original2.0.png"); //original Foto ohne Accident Daten
+  backgroundImg = loadImage("bgAnimation.png");
 }
 
 function setup() {
@@ -39,29 +43,57 @@ function setup() {
   // createCanvas(2700, 1950);
   // createCanvas(4050, 2925);
 
-  //console.log(geodata);
-
   buildingsData = geodata.features;
   roadsData = geodata1.features;
   waterData = geodata2.features;
   accidentData = geodata3.features;
   bankData = geodata4.features;
 
-  noLoop();
+  accidentData = geodata3.features;
+  console.log(accidentData.length);
+  // accidentData = accidentData.filter(function (d) {
+  //   return d.properties.AccidentYear != null;
+  // });
+
+  // accidentData = accidentData.filter(function (d) {
+  //   return d.properties.AccidentYear > 1000;
+  // });
+
+  // console.log(accidentData.length);
+
+  let startYear = d3.min(accidentData, function (d) {
+    return +d.properties.AccidentYear;
+  });
+
+  currentYear = startYear;
+
+  console.log(startYear);
+
+  frameRate(1);
 }
 
 function draw() {
-  // old background("#FEE9C1");
-  // background("#EFE2BA");
-  // image(backgroundImg, 0, 0, width, height);
-  background("#6842EF");
+  image(backgroundImg, 0, 0, width, height);
+  // background("#6842EF");
 
   // drawWater();
   // drawBuildings();
   // drawRoads();
-  drawAccident();
+  // drawAccident();
+  drawAccidentAnimation();
   // drawBank();
   drawText();
+
+  if (currentYear < 2021) {
+    currentYear += 0.25;
+  }
+
+  if (record) {
+    //trees-00001.png
+    //trees-00002.png
+    let fileName = "acciddents-" + nf(frameCount, 5);
+    saveCanvas(fileName, "png");
+  }
 }
 
 //water
@@ -73,7 +105,7 @@ function drawWater() {
     let coordinates2 = coordinates[0];
 
     stroke(1);
-    fill("#A3DAD1");
+    fill(163, 218, 209, 90);
     beginShape();
 
     for (let l = 0; l < coordinates2.length; l++) {
@@ -83,7 +115,6 @@ function drawWater() {
       let y = map(coord[1], bounds.top, bounds.bottom, 0, height);
       let r = random(0, 10);
       vertex(x, y);
-      //ellipse(x,y,r,r);
     }
     endShape();
   }
@@ -100,13 +131,11 @@ function drawBuildings() {
 
     // stroke(0);
     noStroke();
-    //old fill("#CFA969");
-    fill("#d79922");
+    fill("#472ea3");
     beginShape();
 
     for (let j = 0; j < coordinates2.length; j++) {
       let coord = coordinates2[j];
-      //console.log(coord);
       let x = map(coord[0], bounds.left, bounds.right, 0, width);
       let y = map(coord[1], bounds.top, bounds.bottom, 0, height);
       let r = random(0, 10);
@@ -123,73 +152,21 @@ function drawRoads() {
     let roadsCoordinates = geodata1.features[j].geometry.coordinates;
     let properties = geodata1.features[j].properties;
 
-    // if (properties.highway == "residential") {
-    //   stroke("#7986CA");
-    //   strokeWeight(0.5);
-    // } else if (properties.highway == "motorway") {
-    //   noFill();
-    //   stroke("#71332F");
-    //   strokeWeight(3);
-    // } else if (properties.highway == "motorway_link") {
-    //   noFill();
-    //   stroke("#94524d");
-    //   strokeWeight(1.5);
-    // } else if (properties.highway == "primary") {
-    //   stroke("#CC3F31");
-    //   strokeWeight(1);
-    // } else if (properties.highway == "secondary") {
-    //   stroke("#74b9ff");
-    //   strokeWeight(0.5);
-    // } else if (properties.highway == "service") {
-    //   stroke("#C8B4E1");
-    //   strokeWeight(0.5);
-    // } else if (properties.highway == "track") {
-    //   stroke("#27ae60");
-    //   strokeWeight(0.5);
-    // } else if (properties.highway == "path") {
-    //   stroke("#27ae60");
-    //   strokeWeight(0.5);
-    // } else if (properties.highway == "steps") {
-    //   stroke("#c0392b");
-    //   strokeWeight(0.5);
-    // } else if (properties.highway == "cycleway") {
-    //   stroke("#636e72");
-    //   strokeWeight(0.5);
-    // } else if (properties.highway == "pedestrian") {
-    //   stroke("#6D214F");
-    //   strokeWeight(0.5);
-    // } else if (properties.highway == "footway") {
-    //   stroke("#2C3A47");
-    //   strokeWeight(0.5);
-    // } else {
-    //   stroke("#CAD3C8");
-    //   strokeWeight(0.3);
-    // }
-
     if (properties.highway == "motorway") {
       strokeWeight(3);
     } else {
-      strokeWeight(1);
+      strokeWeight(1.5);
     }
-
-    // stroke("#2ACCC8");
-    stroke("#2ACCC8");
+    stroke(42, 204, 200, 90);
     noFill();
-    // fill('rgba(255, 213, 0, 0.1)');
-    // noStroke();
+
     beginShape();
     for (let k = 0; k < roadsCoordinates.length; k++) {
       let roadsCoord = roadsCoordinates[k];
-      //console.log(coord);
       let x = map(roadsCoord[0], bounds.left, bounds.right, 0, width);
       let y = map(roadsCoord[1], bounds.top, bounds.bottom, 0, height);
-      // let r = random(0, 10);
-      // let r = 3;
+
       vertex(x, y);
-      // ellipse(x, y, r);
-      // ellipse(x, y, r*2);
-      // ellipse(x,y,r*3)
-      // ellipse(x, y, r*4);
     }
     endShape();
   }
@@ -202,41 +179,43 @@ function drawAccident() {
     let accidentCoordinates = geodata3.features[i].geometry.coordinates;
     let properties = geodata3.features[i].properties;
 
-    // if (properties.AccidentWeekDay_de == "Freitag") {
-    //   fill("#ff4adb")
-    //   strokeWeight(0.2)
-    // } else if (properties.AccidentYear >= "2017") {
-    //   fill("#00d9ff")
-    //   strokeWeight(0.2)
-    // } else {
-    //   stroke(1);
-    //   strokeWeight(0.1);
-    //   fill("red");
-    // }
-
-    // console.log(accidentCoordinates)
-    // fill(104, 66, 239); //violett
-    fill("#f13c20"); //rot
+    fill("#FFD804"); //rot
     strokeWeight(0.6);
-    stroke(255);
-    // noStroke();
-    // for (let j = 0; j < accidentCoordinates.length; j++) {
-    //   let accidentCoord = accidentCoordinates[j];
+    stroke(0);
 
     let x = map(accidentCoordinates[0], bounds.left, bounds.right, 0, width);
     let y = map(accidentCoordinates[1], bounds.top, bounds.bottom, 0, height);
     // let r = random(0, 10);
-    let r = 7;
+    let r = 5.5;
+    ellipse(x, y, r);
+  }
+}
 
-    //ändern des Design der Tastenkombi
-    if (properties.AccidentWeekDay_de == filter) {
-      ellipse(x, y, r);
+function drawAccidentAnimation() {
+  console.log("drawAccidentAnimation");
+  for (let i = 0; i < accidentData.length; i++) {
+    // console.log(i);
+    let accidentObject = accidentData[i];
+    console.log(accidentObject);
+
+    if (accidentObject.properties.AccidentYear < currentYear) {
+      let coordinates = accidentObject.geometry.coordinates;
+      let lat = coordinates[1];
+      let lon = coordinates[0];
+
+      let x = map(lon, bounds.left, bounds.right, 0, width);
+      let y = map(lat, bounds.top, bounds.bottom, 0, height);
+
+      // let age = currentYear - accidentObject.properties.AccidentYear;
+
+      let r = 7;
+
+      fill("#FFD804"); //rot
+      strokeWeight(0.6);
+      stroke(0);
+
+      ellipse(x, y, r, r);
     }
-
-    // if (properties.AccidentWeekDay_de == "Sonntag") {
-    //   ellipse(x, y, r);
-    // }
-    // }
   }
 }
 
@@ -265,38 +244,9 @@ function drawText() {
   let x = 100;
   let y = 100;
 
-  fill(0, 0, 0, 40);
   noStroke();
-  rect(x, y, 200, 500, 10);
-  textSize(25);
-  noStroke();
-  fill("Black");
-  text("Klicke:", x + 20, y + 40);
-
-  textSize(20);
-  text(
-    "1 für Montag" +
-      "\n" +
-      "\n" +
-      "2 für Dienstag" +
-      "\n" +
-      "\n" +
-      "3 für Mittwoch" +
-      "\n" +
-      "\n" +
-      "4 für Donnerstag" +
-      "\n" +
-      "\n" +
-      "5 für Freitag" +
-      "\n" +
-      "\n" +
-      "6 für Samstag" +
-      "\n" +
-      "\n" +
-      "7 für Sonntag",
-    x + 20,
-    y + 80
-  );
+  fill("#FFD804");
+  text(currentYear, x / 2, y / 2);
 }
 
 //export
@@ -304,22 +254,6 @@ function drawText() {
 function keyTyped() {
   // console.log("saving...");
   // saveCanvas("p5map", "png");
-
-  if (key == "1") {
-    filter = "Montag";
-  } else if (key == "2") {
-    filter = "Dienstag";
-  } else if (key == "3") {
-    filter = "Mittwoch";
-  } else if (key == "4") {
-    filter = "Donnerstag";
-  } else if (key == "5") {
-    filter = "Freitag";
-  } else if (key == "6") {
-    filter = "Samstag";
-  } else if (key == "7") {
-    filter = "Sonntag";
-  }
 
   if (key == "d") {
     saveCanvas("accidentMap", "png");
